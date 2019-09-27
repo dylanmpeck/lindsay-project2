54225325fab0452b7c6abc34dd6e470e255c9120
Dylan Peck <dpeck@student.42.us.org>
2019-09-27 12:40:25 -0700

    Update PROJECT2.py
    Conditional statements are ambiguous and don't seem to work with pandas. Using nested numpy.Where https://docs.scipy.org/doc/numpy/reference/generated/numpy.where.html seems to work in this context.

--- PROJECT2.py
+++ PROJECT2.py
@@ -11,21 +11,13 @@ import numpy as np
 raw_data=pd.read_excel('Module 2 Project_Project Performance_v1(2).xlsx')
 df= pd.DataFrame(raw_data, columns=['Quality Score','Process Days','Project Cost'])
 def prob(x,y,z):
-    if x >500 & y<13 & z<234000:
-        p=7
-    elif x <= 500 &y<13 & z<234000:
-        p=6
-    elif x > 500 & y >=13 & z<234000:
-        p=5
-    elif x >500 & y <13 & z>=234000:
-        p=4
-    elif x <=500 & y >=13 & z<234000:
-        p=3
-    elif x<=500 & y<13 & z>=234000:
-        p=2
-    elif x >500 & y >=13 & z>=234000:
-        p=1
-    else: p=0
+    p = np.where(x > 500 & y < 13 & z < 234000, 7,
+                 np.where(x <= 500 &y<13 & z<234000, 6,
+                          np.where(x > 500 & y >=13 & z<234000, 5,
+                                   np.where(x <=500 & y >=13 & z<234000, 4
+                                            np.where(x <=500 & y >=13 & z<234000, 3,
+                                                     np.where(x<=500 & y<13 & z>=234000, 2,
+                                                              np.where(x >500 & y >=13 & z>=234000, 1, 0)))))))
     return p
 df['p']= prob(df['Quality Score'],df['Process Days'],df['Project Cost'])
 df
